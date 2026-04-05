a1f9c27d01cd14ed41a9bd47e83ccffbe266772d
Mandeep Kaur <mandeep723@gmail.com>
2026-04-05 01:20:54 -0500

    feat: add service layer and fix some errors in other files.

---
+++ src/api/v1/controllers/movieController.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express";
+import * as movieService from "../services/movieService";
+
+export const getAllMovies = async (req: Request, res: Response) => {
+    try {
+        const movies = await movieService.getAllMovies();
+        res.status(200).json(movies);
+    } catch (error: unknown) {
+        res.status(500).json({ message: "Failed to fetch movies" });
+    }
+};
+
+export const getMovieById = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id as string;
+
+        const movie = await movieService.getMovieById(id);
+
+        if (!movie) {
+            return res.status(404).json({ message: "Movie not found" });
+        }
+
+        res.status(200).json(movie);
+    } catch (error: unknown) {
+        res.status(500).json({ message: "Failed to fetch movie" });
+    }
+};
+
+export const createMovie = async (req: Request, res: Response) => {
+    try {
+        const movieData = req.body;
+
+        const newMovie = await movieService.createMovie(movieData);
+
+        res.status(201).json(newMovie);
+    } catch (error: unknown) {
+        res.status(500).json({ message: "Failed to create movie" });
+    }
+};
+
+export const updateMovie = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id as string;
+        const updateData = req.body;
+
+        const updatedMovie = await movieService.updateMovie(id, updateData);
+
+        if (!updatedMovie) {
+            return res.status(404).json({ message: "Movie not found" });
+        }
+
+        res.status(200).json(updatedMovie);
+    } catch (error: unknown) {
+        res.status(500).json({ message: "Failed to update movie" });
+    }
+};
+
+export const deleteMovie = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id as string;
+
+        const isDeleted = await movieService.deleteMovie(id);
+
+        if (!isDeleted) {
+            return res.status(404).json({ message: "Movie not found" });
+        }
+
+        res.status(200).json({ message: "Movie deleted successfully" });
+    } catch (error: unknown) {
+        res.status(500).json({ message: "Failed to delete movie" });
+    }
+};
