3389a0ec295d142c668e353aa3ab927fa0a3c855
Mandeep Kaur <mandeep723@gmail.com>
2026-04-08 19:16:58 -0500

    feat: add swagger-ui in routes.

--- src/api/v1/routes/movieRoutes.ts
+++ src/api/v1/routes/movieRoutes.ts
@@ -1,37 +1,84 @@
-import express, { Router } from "express";
+import { Router } from "express";
 import * as movieController from "../controllers/movieController";
-import { validateRequest } from "../middleware/validateRequest";
-import { movieSchemas } from "../validations/movieValidation";
-
-const router: Router = express.Router();
-
-router.post(
-    "/",
-    validateRequest(movieSchemas.create),
-    movieController.createMovie
-);
-
-router.get(
-    "/",
-    movieController.getAllMovies
-);
-
-router.get(
-    "/:id",
-    validateRequest(movieSchemas.getById),
-    movieController.getMovieById
-);
-
-router.put(
-    "/:id",
-    validateRequest(movieSchemas.update),
-    movieController.updateMovie
-);
-
-router.delete(
-    "/:id",
-    validateRequest(movieSchemas.delete),
-    movieController.deleteMovie
-);
-
+ 
+const router = Router();
+ 
+/**
+ * @swagger
+ * /movies:
+ *   get:
+ *     summary: Get all movies
+ *     tags: [Movies]
+ *     responses:
+ *       200:
+ *         description: Movies fetched successfully
+ */
+router.get("/", movieController.getAllMovies);
+ 
+/**
+ * @swagger
+ * /movies/{id}:
+ *   get:
+ *     summary: Get movie by ID
+ *     tags: [Movies]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Movie fetched successfully
+ */
+router.get("/:id", movieController.getMovieById);
+ 
+/**
+ * @swagger
+ * /movies:
+ *   post:
+ *     summary: Create a new movie
+ *     tags: [Movies]
+ *     responses:
+ *       201:
+ *         description: Movie created successfully
+ */
+router.post("/", movieController.createMovie);
+ 
+/**
+ * @swagger
+ * /movies/{id}:
+ *   put:
+ *     summary: Update a movie
+ *     tags: [Movies]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Movie updated successfully
+ */
+router.put("/:id", movieController.updateMovie);
+ 
+/**
+ * @swagger
+ * /movies/{id}:
+ *   delete:
+ *     summary: Delete a movie
+ *     tags: [Movies]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Movie deleted successfully
+ */
+router.delete("/:id", movieController.deleteMovie);
+ 
 export default router;
