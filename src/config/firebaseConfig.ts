6f8a8c7dc6812c2fab1f0f70c90c07f0520f3924
Jaskarandeep Bhandol <jbhandol@rrc.ca>
2026-04-15 16:26:04 -0500

    feat: configure firebase admin for authentication and role management.

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,18 +1,11 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
-import { getAuth, Auth } from "firebase-admin/auth";
-import { getFirestore, Firestore } from "firebase-admin/firestore";
-import { readFileSync } from "node:fs";
-import { join } from "node:path";
-
-const serviceAccount = JSON.parse(
-    readFileSync(join(process.cwd(), "credentials.json"), "utf8")
-) as ServiceAccount;
-
+import { getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase-admin/auth";
+import serviceAccount from "../../credentials.json";
+ 
 initializeApp({
-    credential: cert(serviceAccount),
+  credential: cert(serviceAccount as ServiceAccount),
 });
-
-const auth: Auth = getAuth();
-const db: Firestore = getFirestore();
-
-export { auth, db };
+ 
+export const db = getFirestore();
+export const auth = getAuth();
